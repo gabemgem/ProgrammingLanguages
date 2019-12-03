5526031458834fa3596b1b52f5a4de2ee9ff92fe
Gabe Maayan <gabemgem@gmail.com>
2019-12-02 21:28:19 -0500

    Increased efficiency

--- HW3/part_1.pl
+++ HW3/part_1.pl
@@ -7,27 +7,15 @@ between(0,10000,N4),
 interestingSet(N1,N2,N3,N4),
 output_list([N1,N2,N3,N4]).
 
-%% Determines if an individual integer is possible to be in the set
-possibleInd(D) :- 
-integer(D),
-between(0,10000,D),
-D =\= 1,
-D =\= 3,
-D =\= 8,
-D =\= 120.
-
-%% Determines if 4 integers could be in the set
-possible(D1,D2,D3,D4) :-
-possibleInd(D1),
-possibleInd(D2),
-possibleInd(D3),
-possibleInd(D4),
-D1 =\= D2,
-D1 =\= D3,
-D1 =\= D4,
-D2 =\= D3,
-D2 =\= D4,
-D3 =\= D4.
+%% Checks that every element in a list is unique and allowed
+unique([]).
+unique([X|Y]) :- 
+\+ memberchk(X,Y),
+X =\= 1,
+X =\= 3,
+X =\= 8,
+X =\= 120,
+unique(Y).
 
 %% Returns true if the sqrt of N is an integer
 square(N) :-
@@ -41,7 +29,7 @@ square(C).
 
 %% Sets up a possible set and tests each pair in the set for interestingness
 interestingSet(N1,N2,N3,N4) :-
-possible(N1,N2,N3,N4),
+unique([N1,N2,N3,N4]),
 inSet(N1,N2),
 inSet(N1,N3),
 inSet(N1,N4),
